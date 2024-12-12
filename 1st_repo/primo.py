74bb4de2fecd3518593bfbbddd36980561247466
GuiSartori <guilherme.sartori@live.com>
2024-12-12 12:01:12 -0300

    first commit

---
+++ 1st_repo/primo.py
@@ -0,0 +1,11 @@
+#Encontra números primos
+
+num=100
+count = 0
+
+for i in range (1,num+1):
+    if num%i == 0:
+        count += 1
+print(count)
+if count == 2:
+    print(f'{num} é um número primo.')
